6e63e49e3fb876b455d2bb9cb23ba249e11e882b
Hao Li <lihaosky@hotmail.com>
2012-09-11 13:37:36 -0400

    retail shop. Some problems exist

---
+++ Proj1/retail_shop.c
@@ -0,0 +1,91 @@
+/* Retail shop application and multiple function definitions */
+
+#inlcude <stdio.h>
+#include "queue.h"
+#include "synch.h"
+
+/* Current serial number, starting from 0*/
+int serial_num = 0;
+
+/* Phone queue. Unpacked by employee */
+queue_t phone_queue;
+
+/* Employee number */
+int employee_num = 5;
+
+/* Customer number */
+int customer_num = 10;
+
+/* Serial number semphore */
+semaphore_t serial_sem;
+
+/*Phone queue semphore */
+semaphore_t queue_sem;
+
+
+phone_t phone_create() {
+	phone_t phone = (phone_t) malloc(sizeof(struct phone));
+	if (phone == NULL) {
+		printf("Can't unpack a new phone\n");
+		return NULL;
+	}
+	
+	semaphore_P(serial_sem);
+	phone->serial_num = serial_num++;
+	semaphore_V(serial_sem);
+}
+
+static void employee(int* arg) {
+	phone_t phone = NULL;
+	
+	while (1) {
+		phone = phone_create();
+		if (phone) {
+			semaphore_P(queue_sem);
+			queue_append(phone_quque, phone);
+			printf("Employee %d unpacked phone %d\n", *arg, phone->serial_num);
+			semaphore_V(queue_sem);
+		}
+	}
+}
+
+static void customer(int* arg) {
+	phone_t phone = NULL;
+	int i;
+	
+	for (i = 0; i < customer_num; i++) {
+		while (phone == NULL) {
+			semaphore_P(queue_sem);
+			queue_dequeue(phone_queue, &phone);
+			semaphore_V(queue_sem);
+		}
+
+		printf("Customer %d got phone %d\n", *arg, phone->serial_num);
+		free(phone);
+	}
+}
+
+static void start(int* arg) {
+	int i;
+	
+	minithread_fork(customer, NULL);
+	minithread_yield();
+	
+	for (i = 0; i < employee_num; i++) {
+		minithread_fork(employee, &i);
+	}
+	
+}
+
+void main(int argc, char** argv) {
+	phone_queue = queue_new();
+	if (phone_queue == NULL) {
+		printf("Can't create phone queue!\n");
+		return;
+	}
+	
+	semaphore_initialize(serial_sem, 1);
+	semaphore_initialize(queue_sem, 1);
+	
+	minithread_system_initialize(start, NULL);
+}

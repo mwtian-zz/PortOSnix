c13e9652de82e2deff6f6824e89af0e52d741f38
mtian <mwtian@gmail.com>
2012-09-23 23:22:26 -0400

    Project 1 finalized.

--- P1/retail_shop_1.c
+++ P1/retail_shop_1.c
@@ -13,19 +13,22 @@ int serial_num = 0;
 queue_t phone_queue;
 
 /* Employee number */
-int employee_num = 20;
+int employee_num = 88;
 /* Customer number */
-int customer_num = 200;
+int customer_num = 2090;
 /* Employee id */
 int employee_id = 0;
 /*Customer id */
 int customer_id = 0;
+/* Empty semaphore */
+semaphore_t empty_sem;
+/* Full semaphore */
+semaphore_t full_sem;
 /* Customer semaphore */
 semaphore_t customer_sem;
-/* Employee semaphore */
-semaphore_t employee_sem;
 
-phone_t phone_create() {
+phone_t
+phone_create() {
     phone_t new_phone = (phone_t) malloc(sizeof(struct phone));
     if (new_phone == NULL) {
         printf("Can't unpack a new phone\n");
@@ -36,49 +39,54 @@ phone_t phone_create() {
     return new_phone;
 }
 
-static int employee(int* arg) {
+static int
+employee(int* arg) {
     phone_t new_phone = NULL;
     int id = employee_id++;
 
     while (1) {
+        /* Check if there is any customer in need of a phone */
+        semaphore_P(customer_sem);
+
+        /* Wait if buffer full */
+        semaphore_P(empty_sem);
         new_phone = phone_create();
-        if (new_phone) {
-            /* Check if there is any customer in need of a phone */
-            semaphore_P(customer_sem);
-            /* Unpack the phone then */
+        if (NULL != new_phone) {
             queue_append(phone_queue, new_phone);
-
             printf("Employee %d unpacked phone %d\n", id, new_phone->serial_num);
-            /* Tell the customer the phone is ready */
-            semaphore_V(employee_sem);
-
-            minithread_yield();
         }
+        /* Signal a phone is ready */
+        semaphore_V(full_sem);
+
+        minithread_yield();
     }
 
     return 0;
 }
 
-static int customer(int* arg) {
+static int
+customer(int* arg) {
     phone_t new_phone = NULL;
     int id = customer_id++;
 
     printf("Customer %d is waiting for a phone\n", id);
 
-    /* Tell employee there is a customer */
+    /* Signal employee there is a customer */
     semaphore_V(customer_sem);
-    /* Check if a phone is unpacked */
-    semaphore_P(employee_sem);
-    /* Get the phone */
-    queue_dequeue(phone_queue, (void**)&new_phone);
 
+    /* Wait if no unpacked phone */
+    semaphore_P(full_sem);
+    queue_dequeue(phone_queue, (void**)&new_phone);
     printf("Customer %d got phone %d\n", id, new_phone->serial_num);
     free(new_phone);
+    /* Signal a buffer slot is open */
+    semaphore_V(empty_sem);
 
     return 0;
 }
 
-static int start(int* arg) {
+static int
+start(int* arg) {
     int i;
 
     for (i = 0; i < employee_num; i++) {
@@ -92,18 +100,21 @@ static int start(int* arg) {
     return 0;
 }
 
-int main(int argc, char** argv) {
+int
+main(int argc, char** argv) {
     phone_queue = queue_new();
     if (phone_queue == NULL) {
         printf("Can't create phone queue!\n");
-        return;
+        return 0;
     }
 
     /* Semaphore creation */
+    empty_sem = semaphore_create();
+    full_sem = semaphore_create();
     customer_sem = semaphore_create();
-    employee_sem = semaphore_create();
+    semaphore_initialize(empty_sem, employee_num);
+    semaphore_initialize(full_sem, 0);
     semaphore_initialize(customer_sem, 0);
-    semaphore_initialize(employee_sem, 0);
 
     /* Start main thread */
     minithread_system_initialize(start, NULL);

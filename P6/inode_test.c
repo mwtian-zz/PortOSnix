9ac7ef9d99475bd68069bf6ba88443c09bbf90ab
Hao Li <lihaosky@hotmail.com>
2012-11-30 23:48:18 -0500

    Multiple inodes in one block...

--- P6/inode_test.c
+++ P6/inode_test.c
@@ -5,5 +5,8 @@ int main() {
 	printf("Size of inode is %lu\n", sizeof(struct inode));
 	printf("Inodes per block is %d\n", INODE_PER_BLOCK);
 	printf("Inode 32 is in block %d\n", INODE_TO_BLOCK(32));
+	printf("Inode 32 offset is %d\n", INODE_OFFSET(32));
+	printf("Inode 33 offset is %d\n", INODE_OFFSET(33));
+	printf("Inode 100 offset is %d\n", INODE_OFFSET(100));
 	return 0;
 }
